41c0984dabaaccddd5683752090c7e79511c755c
Arion <Arion@DESKTOP-9BU80RS>
2023-11-06 18:01:54 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.Write("Введите n:");
+    int n = int.Parse(Console.ReadLine());
+      double S = 0;
+    for (int k = 1; n <= k; k++)
+    {
+            if (k != (-2)) S += Math.Pow((-1),k)*(Math.Pow(k,3)-27)/(3*(k+2));
+    }
+    Console.WriteLine($"S={S:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
